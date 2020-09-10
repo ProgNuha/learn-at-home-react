6f377468310a678ee23964c232bb9f196084b2be
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2020-09-10 14:26:12 +0700

    add condition at follow button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.css";
 
 export default function App() {
   const username = "mufidanuha";
+  const [isFollowed, setIsFollowed] = useState(false);
 
   return (
     <>
       <img src="https://via.placeholder.com/300" alt="profile picture" />
       <div>@{username}</div>
-      <button>Follow</button>
+      <button onClick={() => setIsFollowed(!isFollowed)}>
+        {isFollowed ? "Unfollow":"Follow"}
+        </button>
       <div>Posts</div>
       <div>Follower</div>
       <div>Following</div>
