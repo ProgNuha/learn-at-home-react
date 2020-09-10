32439f95ec8f77ffd9bf1e38ef37367ae1cfe41a
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2020-09-10 14:21:27 +0700

    add content instagram profile

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const username = "mufidanuha";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src="https://via.placeholder.com/300" alt="profile picture" />
+      <div>@{username}</div>
+      <button>Follow</button>
+      <div>Posts</div>
+      <div>Follower</div>
+      <div>Following</div>
+      <div>Bio</div>
+    </>
   );
 }
-
-export default App;
