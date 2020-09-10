81bf711b26933de508c554350e4fe08eceb14379
ProgNuha <mufida.nuha.tif17@polban.ac.id>
2020-09-10 14:30:10 +0700

    add input search

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import "./styles.css";
 export default function App() {
   const username = "mufidanuha";
   const [isFollowed, setIsFollowed] = useState(false);
+  const [inputAccount, setInputAccount] = useState("");
 
   return (
     <>
+      <input onChange={({target: {value} }) => {
+        setInputAccount(value);
+      }}
+        placeholder="search profile"
+      />
+      <div>Search Result for Account: {inputAccount}</div>
       <img src="https://via.placeholder.com/300" alt="profile picture" />
       <div>@{username}</div>
       <button onClick={() => setIsFollowed(!isFollowed)}>
